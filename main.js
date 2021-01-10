3a7596b8b9f4f3f5dc9c9a5984dcd021e5deb60c
Timothy Do <timothydobsa@gmail.com>
2021-01-09 22:19:57 -0800

    ok

--- main.js
+++ main.js
@@ -21,7 +21,26 @@ function q2b() {
   transition("question2","question3");
 }
 
+var a3 = b3 = c3 = 0;
+function q3a(answer) {
+  var checklist = [];
+  if(answer == "CB Answer1") {
+    if(a3 == 0)
+    checklist[0] = answer;
+  }
+  else if(answer == "CB Answer2" && b3 == 0) {
+    checklist[1] = answer;
+    b3++;
+  }
+  else if(answer == "CB Answer3" && c3 == 0)
+
+}
+
+function q3b() {
+  transition("question3","Welcome");
+}
+
 function transition(element1,element2) {
-  document.getElementById('element1').style.display = 'none';
-  document.getElementById('element2').style.display = 'block';
+  document.getElementById(element1).style.display = 'none';
+  document.getElementById(element2).style.display = 'block';
 }
