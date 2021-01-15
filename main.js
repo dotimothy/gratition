b27e5597f1c2e00adced2ac60efe38b6169fefd5
Timothy Do <timothydobsa@gmail.com>
2021-01-15 01:15:51 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var answers = ["","","","","","","","","","",""];
-var questions = ["question1","question2","question3","question4","question5","question6","question7","question8","question9","question10","question11","result"];
-var elements = ["Welcome","question1","question2","question3","question4","question5","question6","question7","question8","question9","question10","question11","result"];
+var questions = ["question1","question2","question3","question4","question5","question6","question7","question8","question9","question10","question11"];
+var qc = ["What is your full name?","How are you feeling today?","What did you accomplish since waking up?","Did these accomplishments make you happy?","Name One Thing You Would Like to Complete Today.","How Many Hours of Sleep Did You Get Last Night?","What Time Did You Go to Bed Last Night?","What Made You Lose Track of Time Today?","Fill in the Blank: Today I was so...","When I go to sleep, I want to feel...","When Time Are You Free Today?"];
 var msg = new SpeechSynthesisUtterance();
 var volume = 1;
 msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name === "Google UK English Male"; })[0];
@@ -15,11 +15,23 @@ function mute() {
   msg.volume = volume;
 }
 
+function checkSpeech() {
+  var speak = 0;
+  for(var i = 0; i < questions.length; i++) {
+    var element = document.getElementById(questions[i]);
+    if(element.classList.contains("fade-in") && !(speak)) {
+      speakText(qc[i]);
+      speak = 1;
+    }
+  }
+  speak = 0;
+}
+
 
 function startGratiton() {
 //separate function for transitions
   transition("Welcome","question1");
-  speakText("What is your full name?");
+  //speakText("What is your full name?");
 }
 
 // For now, it's the female Google voice but can make it adjustable per user settings soon!
@@ -34,7 +46,7 @@ function q1() {
   var input = document.getElementById("answer1").value;
   answers[0] = input;
   transition("question1","question2");
-  speakText("How are you feeling today?");
+  //speakText("How are you feeling today?");
 }
 
 function q2a(answer) {
@@ -43,14 +55,14 @@ function q2a(answer) {
 
 function q2b() {
   transition("question2","question3");
-  speakText("What did you accomplish since waking up?");
+  //speakText("What did you accomplish since waking up?");
 }
 
 function q3() {
   var input = document.getElementById("answer3").value;
   answers[2] = input;
   transition("question3","question4");
-  speakText("Did these accomplishments make you happy?");
+  //speakText("Did these accomplishments make you happy?");
 }
 
 function q4a(answer) {
@@ -58,14 +70,14 @@ function q4a(answer) {
 }
 
 function q4b() {
-  speakText("Name One Thing You Would Like to Complete Today.");
+  //speakText("Name One Thing You Would Like to Complete Today.");
   transition("question4","question5");
 }
 
 function q5() {
   var input = document.getElementById("answer5").value;
   answers[4] = input;
-  speakText("How Many Hours of Sleep Did You Get Last Night?");
+  //speakText("How Many Hours of Sleep Did You Get Last Night?");
   transition("question5","question6");
 }
 
@@ -74,7 +86,7 @@ function q6a(answer) {
 }
 
 function q6b() {
-  speakText("What Time Did You Go to Bed Last Night?");
+  //speakText("What Time Did You Go to Bed Last Night?");
   transition("question6","question7");
 }
 
@@ -83,7 +95,7 @@ function q7a(answer) {
 }
 
 function q7b() {
-  speakText("What Made You Lose Track of Time Today?");
+  //speakText("What Made You Lose Track of Time Today?");
   transition("question7","question8");
 }
 
@@ -92,21 +104,21 @@ function q8a(answer) {
 }
 
 function q8b() {
-  speakText("Fill in the Blank: Today I was so...");
+  //speakText("Fill in the Blank: Today I was so...");
   transition("question8","question9");
 }
 
 function q9() {
   var input = document.getElementById("answer9").value;
   answers[8] = input;
-  speakText("When I go to sleep, I want to feel...");
+  //speakText("When I go to sleep, I want to feel...");
   transition("question9","question10");
 }
 
 function q10() {
   var input = document.getElementById("answer10").value;
   answers[9] = input;
-  speakText("When Time Are You Free Today?");
+  //speakText("When Time Are You Free Today?");
   transition("question10","question11");
 }
 
@@ -114,7 +126,7 @@ async function q11() {
   var input = document.getElementById("answer11").value;
   answers[10] = input;
   transition("question11","result");
-  msg.rate = 1.5;
+  msg.rate = 1.1;
   await sleep(250);
   await madlib();
   showParty();
@@ -136,10 +148,10 @@ async function sleep(ms) {
 
 async function madlib() {
     var element = document.getElementById("madlib");
-    var delay = 25;
+    var delay = 30;
     var speakMessage = `Hello |${answers[0]}|! Today you were feeling |${answers[1]}|. After waking up you managed to accomplish |${answers[2]}| which made you |${answers[3]}| happy. The most notable thing that you completed today was |${answers[4]}|, `;
     speakMessage += `which was very impressive considering you got |${answers[5]}| of sleep after going to bed at |${answers[6]}| last night. Today may have been a bit stressful due to |${answers[7]}| that made you lose track of time, but you got through it! Overall, you were so |${answers[8]}| `;
-    speakMessage += `today, but tomorrow is a new day! When you go to sleep you want to feel |${answers[9]}| and after taking time for yourself at |${answers[10]}|, you definitely deserve to feel that way! Thank you for using Gratition, and enjoy the rest of your day!`;
+    speakMessage += `today, but tomorrow is a new day! When you go to sleep you want to feel |${answers[9]}| and after taking time for yourself at |${answers[10]}|, you definitely deserve to feel that way! Thank you for using Graatition, and enjoy the rest of your day!`;
     speakText(speakMessage);
 
     // so here I used template literals instead of concatenating variables and string with double quotes which can be messy.
@@ -189,4 +201,5 @@ function transition(outElement,inElement) {
   inE.classList.add('fade-in');
   // inE.style.display = "block";
   inE.removeAttribute('hidden');
+  checkSpeech();
 }
