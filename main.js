8af213db4969aa5597ef51c5dec52fabfae886e0
Timothy Do <timothydobsa@gmail.com>
2021-01-14 23:06:04 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var answers = ["","","","","","","","","","",""];
 var questions = ["question1","question2","question3","question4","question5","question6","question7","question8","question9","question10","question11","question12"];
 
+
 var on = true;
 function mute() {
   if(on == true) {
@@ -12,6 +13,7 @@ function mute() {
     on = true;
   }
 }
+
 
 function startGratiton() {
 //separate function for transitions
